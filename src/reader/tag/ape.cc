0794ca4df79500bedf4f1b391a9b2ba96f603ccb
Jia Jen Ng <jiajen.git@gmail.com>
2018-07-22 19:21:26 +0800

    Constants

--- src/reader/tag/ape.cc
+++ src/reader/tag/ape.cc
@@ -4,6 +4,20 @@ namespace Mp3Edit {
 namespace ReaderTag {
 namespace Ape {
 
+namespace {
+
+using Filesystem::readBytes;
+
+const int kTagHeaderLength = 32;
+const int kTagSizeStartPos = 12;
+const int kTagSizeEndPos = 15;
+
+// Must be zero
+const int kTagReservedStartPos = 24;
+const int kTagReservedStartPos = 31;
+
+}  // namespace
+
 int seekHeaderEnd(Filesystem::FileStream& file_stream, int seek) {
 
 }
