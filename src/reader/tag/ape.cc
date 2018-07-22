0d07b920c76fff7bb1ac4304df55775bd28263dc
Jia Jen Ng <jiajen.git@gmail.com>
2018-07-22 19:04:42 +0800

    Structure

---
+++ src/reader/tag/ape.cc
@@ -0,0 +1,17 @@
+#include "mp3edit/src/reader/tag/ape.h"
+
+namespace Mp3Edit {
+namespace ReaderTag {
+namespace Ape {
+
+int seekHeaderEnd(Filesystem::FileStream& file_stream, int seek) {
+
+}
+
+int seekFooterStart(Filesystem::FileStream& file_stream, int seek) {
+
+}
+
+}  // namespace Ape
+}  // namespace ReaderTag
+}  // namespace Mp3Edit
