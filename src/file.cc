fa5d02a6c6c2c9d2f946995422324c26b7d4506c
Jia Jen Ng <jiajen.git@gmail.com>
2018-07-30 23:21:03 +0800

    Enable flac skipping

--- src/file.cc
+++ src/file.cc
@@ -91,12 +91,12 @@ void File::readMetaData(Filesystem::FileStream& file_stream,
         seek_end = Ape::seekFooterStart(file_stream, seek_end);
         break;
       case FileType::kFlac:
-        // seek = VorbisFlac::seekHeaderEnd(file_stream, seek_start);
+        seek = VorbisFlac::seekHeaderEnd(file_stream, seek_start);
         if (seek != seek_start) {
           // TODO parse vorbis flac
           seek_start = seek;
         }
-        // seek_end = VorbisFlac::seekFooterStart(file_stream, seek_end);
+        seek_end = VorbisFlac::seekFooterStart(file_stream, seek_end);
         break;
       case FileType::kOgg:
         // seek = VorbisOgg::seekHeaderEnd(file_stream, seek_start);
