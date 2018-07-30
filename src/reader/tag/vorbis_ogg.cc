00f22c17737d58dfbab7b244b986ccb38d78d9eb
Jia Jen Ng <jiajen.git@gmail.com>
2018-07-31 02:50:29 +0800

    Constants for initial scanning

--- src/reader/tag/vorbis_ogg.cc
+++ src/reader/tag/vorbis_ogg.cc
@@ -1,5 +1,9 @@
 #include "mp3edit/src/reader/tag/vorbis_ogg.h"
 
+#include <cstring>
+
+#include <exception>
+
 namespace Mp3Edit {
 namespace ReaderTag {
 namespace VorbisOgg {
@@ -8,9 +12,15 @@ namespace {
 
 using Filesystem::readBytes;
 
+const int kFirstPageLength = 58;
+const int kPageHeaderPrefixLength = 27;
+
 }  // namespace
 
 int seekHeaderEnd(Filesystem::FileStream& file_stream, int seek) {
+  Bytes header;
+  readBytes(file_stream, seek, kFirstPageLength + kPageHeaderPrefixLength,
+            header);
   // TODO
   return seek;
 }
