928cdfdd3aa985d0013b04e8402c274eb15c043e
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-09 21:10:23 +0800

    Only parse id3v2.3 from header

--- src/reader/tag/id3v2_3.cc
+++ src/reader/tag/id3v2_3.cc
@@ -5,9 +5,28 @@
 namespace Mp3Edit {
 namespace ReaderTag {
 namespace Id3v2_3 {
+namespace {
+
+const int kTagHeaderLength = 10;
+
+int getPostHeaderSeek(const Bytes& tag, bool has_extended_header) {
+  // TODO handle extended header
+  return -1;
+}
+
+}  // namespace
 
 void parseTag(const Bytes& tag, std::string& title, std::string& artist,
               std::string& album, int track_num, int track_denum) {
+  bool has_unsync, has_extended_header;
+  int tag_version, unused;
+
+  Bytes header(tag.begin(), tag.begin() + kTagHeaderLength);
+  Id3v2::parseTagHeader(header, false, tag_version, unused,
+                        has_unsync, has_extended_header);
+  if (tag_version != 3) return;
+
+
   // TODO output data from tag
 }
 
