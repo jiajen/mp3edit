6dcd0f194bc0d7633db9dbeff1bb0623247ee808
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-11 21:38:34 +0800

    Extract all needed data

--- src/reader/tag/vorbis_ogg.cc
+++ src/reader/tag/vorbis_ogg.cc
@@ -141,10 +141,28 @@ int seekFooterStart(Filesystem::FileStream&, int seek) {
 Bytes generateTag(Filesystem::FileStream& file_stream, int seek_audio_start,
                   const std::string& title, const std::string& artist,
                   const std::string& album, int track_num, int track_denum) {
+  Bytes header_second_page;
+  readBytes(file_stream, kFirstPageLength, kPageHeaderPrefixLength,
+            header_second_page);
+
+  Bytes segment_table;
+  int number_segments = header_second_page[kNumberPageSegmentsPos];
+  readBytes(file_stream, kFirstPageLength + kPageHeaderPrefixLength,
+            number_segments, segment_table);
+  int page_size = segmentTableToSize(segment_table);
+
   using VorbisShared::generateTag;
   Bytes vorbis_tag = generateTag(title, artist, album,
                                  track_num, track_denum, false);
+
+  int vorbis_setup_size = kFirstPageLength + kPageHeaderPrefixLength +
+                          number_segments + page_size - seek_audio_start;
+  Bytes page_audio_data;
+  readBytes(file_stream, seek_audio_start, vorbis_setup_size, page_audio_data);
+
   // TODO
+  // add 0x03 vorbis in header_second_page + segment_table + <here> + vorbis_tag
+  // when compiling
 }
 
 }  // namespace VorbisOgg
