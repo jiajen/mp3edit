992c887ff050f1d0d66fcc2fd1836c11afe3d406
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-12 20:58:05 +0800

    Fix sanitising filename (fix bug where name is blank after sanitising)

--- src/sanitiser.cc
+++ src/sanitiser.cc
@@ -31,11 +31,13 @@ std::string toValidFilename(const std::string& str) {
   std::string filename, sanitised_filename = str;
   sanitiseString(sanitised_filename);
 
-  size_t strt_idx = 0, nxt_idx, size = str.length();
+  size_t strt_idx = 0, nxt_idx, size = sanitised_filename.length();
   do {
     nxt_idx = sanitised_filename.find_first_of(kInvalidFilenameChars, strt_idx);
     if (nxt_idx == std::string::npos) nxt_idx = size;
-    if (nxt_idx - strt_idx > 0) filename.append(strt_idx, nxt_idx - strt_idx);
+    if (nxt_idx - strt_idx > 0)
+      filename.append(sanitised_filename.data() + strt_idx,
+                      sanitised_filename.data() + nxt_idx);
     strt_idx = nxt_idx + 1;
   } while (nxt_idx != size);
 
