bc70d95f72e111f053cd4b7317ca52e1f5d4f568
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-12 17:14:10 +0800

    Reorder header file function for readers

--- src/reader/tag/id3v2_3.h
+++ src/reader/tag/id3v2_3.h
@@ -11,12 +11,12 @@ namespace Mp3Edit {
 namespace ReaderTag {
 namespace Id3v2_3 {
 
-void parseTag(const Bytes& tag, std::string& title, std::string& artist,
-              std::string& album, int& track_num, int& track_denum);
-
 // Returns the metadata tag portion of an audio file.
 Bytes extractTag(Filesystem::FileStream& file_stream,
                  int seek_tag_start, int seek_tag_end);
+
+void parseTag(const Bytes& tag, std::string& title, std::string& artist,
+              std::string& album, int& track_num, int& track_denum);
 
 // Returns the entire tag
 Bytes generateTag(const std::string& title, const std::string& artist,
--- src/reader/tag/vorbis_ogg.h
+++ src/reader/tag/vorbis_ogg.h
@@ -7,15 +7,15 @@ namespace Mp3Edit {
 namespace ReaderTag {
 namespace VorbisOgg {
 
-void parseTag(const Bytes& tag, std::string& title, std::string& artist,
-              std::string& album, int& track_num, int& track_denum);
+int seekHeaderEnd(Filesystem::FileStream& file_stream, int seek);
 
 // Returns the entire vorbis second page
 // (with 0x03 and vorbis but without Ogg encapsulation).
 Bytes extractTag(Filesystem::FileStream& file_stream,
                  int seek_tag_start, int);
 
-int seekHeaderEnd(Filesystem::FileStream& file_stream, int seek);
+void parseTag(const Bytes& tag, std::string& title, std::string& artist,
+              std::string& album, int& track_num, int& track_denum);
 
 Bytes generateTag(Filesystem::FileStream& file_stream,
                   int seek_ogg_start, int seek_audio_start,
