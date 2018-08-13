6919b5fb6983eb7efe7f09aef389cb75b3e6c30a
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-13 12:07:25 +0800

    Initial namespaces

---
+++ src/reader/audio/mp3.cc
@@ -0,0 +1,9 @@
+#include "mp3edit/src/reader/audio/mp3.h"
+
+namespace Mp3Edit {
+namespace ReaderAudio {
+namespace Mp3 {
+
+}  // namespace Mp3
+}  // namespace ReaderAudio
+}  // namespace Mp3Edit
---
+++ src/reader/audio/mp3.h
@@ -0,0 +1,12 @@
+#ifndef MP3EDIT_SRC_READER_AUDIO_MP3_H_
+#define MP3EDIT_SRC_READER_AUDIO_MP3_H_
+
+namespace Mp3Edit {
+namespace ReaderAudio {
+namespace Mp3 {
+
+}  // namespace Mp3
+}  // namespace ReaderAudio
+}  // namespace Mp3Edit
+
+#endif  // MP3EDIT_SRC_READER_AUDIO_MP3_H_
