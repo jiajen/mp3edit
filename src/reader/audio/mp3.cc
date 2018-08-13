3e5f7687974cab0affa321f2470edbf5b1132878
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-13 13:05:20 +0800

    Scan header from each frame

--- src/reader/audio/mp3.cc
+++ src/reader/audio/mp3.cc
@@ -4,11 +4,21 @@ namespace Mp3Edit {
 namespace ReaderAudio {
 namespace Mp3 {
 
+namespace {
+
+using Filesystem::readBytes;
+
+const int kFrameHeaderLength = 4;
+
+}  // namespace
+
 bool getAudioProperties(Filesystem::FileStream& file_stream,
                         int seek, int audio_end,
                         File::BitrateType& bitrate_type, int& bitrate,
                         int& sampling_rate, File::ChannelMode& channel_mode) {
+  Bytes header;
   while (seek < audio_end) {
+    readBytes(file_stream, seek, kFrameHeaderLength, header);
     // TODO read each frame
   }
 
