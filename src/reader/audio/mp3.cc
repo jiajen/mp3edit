24d0fc32a5b870a41b7456b47d1a3752c392e49f
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-13 12:18:32 +0800

    Structure for function to extract audio properties

--- src/reader/audio/mp3.cc
+++ src/reader/audio/mp3.cc
@@ -4,6 +4,13 @@ namespace Mp3Edit {
 namespace ReaderAudio {
 namespace Mp3 {
 
+bool getAudioProperties(Filesystem::FileStream& file_stream,
+                        int seek, int filesize,
+                        File::BitrateType& bitrate_type, int& bitrate,
+                        int& sampling_rate, File::ChannelMode& channel_mode) {
+  // TODO
+}
+
 }  // namespace Mp3
 }  // namespace ReaderAudio
 }  // namespace Mp3Edit
--- src/reader/audio/mp3.h
+++ src/reader/audio/mp3.h
@@ -1,9 +1,17 @@
 #ifndef MP3EDIT_SRC_READER_AUDIO_MP3_H_
 #define MP3EDIT_SRC_READER_AUDIO_MP3_H_
 
+#include "mp3edit/src/filesystem.h"
+#include "mp3edit/src/file.h"
+
 namespace Mp3Edit {
 namespace ReaderAudio {
 namespace Mp3 {
+
+bool getAudioProperties(Filesystem::FileStream& file_stream,
+                        int seek, int filesize,
+                        File::BitrateType& bitrate_type, int& bitrate,
+                        int& sampling_rate, File::ChannelMode& channel_mode);
 
 }  // namespace Mp3
 }  // namespace ReaderAudio
