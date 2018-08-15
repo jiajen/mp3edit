e53c4f528a120a049c56dc70132f9c89c5af034c
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-15 22:03:45 +0800

    Bitmasks for data

--- src/reader/audio/mp3.cc
+++ src/reader/audio/mp3.cc
@@ -10,6 +10,34 @@ using Filesystem::readBytes;
 
 const int kFrameHeaderLength = 4;
 
+const int kSyncPos = 0;
+const int kSyncSize = 2;
+const char* kSyncBitMask = "\xFF\xE0";
+
+const int kVersionPos = 1;
+const char kVersionBitMask = '\x18';
+const int kVersionShift = 3;
+
+const int kLayerPos = 1;
+const char kLayerBitMask = '\x06';
+const int kLayerShift = 1;
+
+const int kBitratePos = 2;
+const char kBitrateBitMask = '\xF0';
+const int kBitrateShift = 4;
+
+const int kSamplingPos = 2;
+const char kSamplingBitMask = '\x0C';
+const int kSamplingShift = 2;
+
+const int kPaddingPos = 2;
+const char kPaddingBitMask = '\x02';
+const int kPaddingShift = 1;
+
+const int kChannelPos = 3;
+const char kChannelBitMask = '\xC0';
+const int kChannelShift = 6;
+
 }  // namespace
 
 bool getAudioProperties(Filesystem::FileStream& file_stream,
@@ -19,6 +47,7 @@ bool getAudioProperties(Filesystem::FileStream& file_stream,
   Bytes header;
   while (seek < audio_end) {
     readBytes(file_stream, seek, kFrameHeaderLength, header);
+
     // TODO read each frame
   }
 
