d33e956c347a96771b4a879c1a81af85161e83a8
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-18 00:18:50 +0800

    Implement recursive file scanning

--- src/directory.cc
+++ src/directory.cc
@@ -8,17 +8,38 @@
 namespace Mp3Edit {
 namespace Directory {
 
+namespace {
+
+template <class T>
+void pushFileIfValid(const T& entry, bool read_audio_data,
+                     std::vector<File::File>& files) {
+  if (entry.is_regular_file())
+    pushFileIfValid((std::string)entry.path(), read_audio_data, files);
+}
+
+template <>
+void pushFileIfValid<std::string>(const std::string& path, bool read_audio_data,
+                                  std::vector<File::File>& files) {
+  File::FileType filetype = File::getAudioExtension(path);
+  if (filetype != File::FileType::kInvalid)
+    files.emplace_back(path, filetype, read_audio_data);
+}
+
+}  // namespace
+
 std::vector<File::File> getFiles(const std::string& directory,
                                  bool recurse,
                                  bool read_audio_data) {
-  // TODO handle recurse
+  using std::filesystem::directory_iterator;
+  using std::filesystem::recursive_directory_iterator;
+
   std::vector<File::File> files;
-  for (const auto& entry: std::filesystem::directory_iterator(directory)) {
-    if (!entry.is_regular_file()) continue;
-    std::string path = (std::string)entry.path();
-    File::FileType filetype = File::getAudioExtension(path);
-    if (filetype != File::FileType::kInvalid)
-      files.emplace_back(path, filetype, read_audio_data);
+  if (recurse) {
+    for (const auto& entry: recursive_directory_iterator(directory))
+      pushFileIfValid(entry, read_audio_data, files);
+  } else {
+    for (const auto& entry: directory_iterator(directory))
+      pushFileIfValid(entry, read_audio_data, files);
   }
 
   using File::File;
