727183bd24e1de05495aa077a17b9f6ebb08941d
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-17 22:02:07 +0800

    Parse vorbis identification header data

--- src/reader/audio/ogg.cc
+++ src/reader/audio/ogg.cc
@@ -1,10 +1,14 @@
 #include "mp3edit/src/reader/audio/ogg.h"
+#include "mp3edit/src/reader/utility.h"
 
 namespace Mp3Edit {
 namespace ReaderAudio {
 namespace Ogg {
 
 namespace {
+
+using Filesystem::readBytes;
+using Reader::Utility::lEndianToInt;
 
 const int kVorbisIdHeaderPos = 35;
 const int kVorbisIdHeaderSize = 23;
@@ -25,7 +29,6 @@ const int kBitrateMinSize = 4;
 
 const unsigned char kFramingFlag = '\x01';
 const int kFramingFlagPos = 22;
-const int kFramingFlagSize = 1;
 
 }  // namespace
 
@@ -33,7 +36,39 @@ bool getAudioProperties(Filesystem::FileStream& file_stream,
                         int seek, int audio_end,
                         File::BitrateType& bitrate_type, int& bitrate,
                         int& sampling_rate, File::ChannelMode& channel_mode) {
-  // TODO
+  Bytes header;
+  readBytes(file_stream, seek+kVorbisIdHeaderPos, kVorbisIdHeaderSize, header);
+  int version = lEndianToInt(header.begin() + kVersionPos,
+                             header.begin() + kVersionPos + kVersionSize,
+                             false);
+  if (version != kVersion) return false;
+  if (header[kFramingFlagPos] != kFramingFlag) return false;
+
+  int num_channels = lEndianToInt(header.begin() + kNumChannelsPos,
+                                  header.begin() + kNumChannelsPos +
+                                                   kNumChannelsSize,
+                                  false);
+
+  sampling_rate = lEndianToInt(header.begin() + kSamplingRatePos,
+                               header.begin() + kSamplingRatePos +
+                                                kSamplingRateSize,
+                               false);
+
+  int bitrate_max = lEndianToInt(header.begin() + kBitrateMaxPos,
+                                 header.begin() + kBitrateMaxPos +
+                                                  kBitrateMaxSize,
+                                 false);
+
+  int bitrate_nominal = lEndianToInt(header.begin() + kBitrateNominalPos,
+                                     header.begin() + kBitrateNominalPos +
+                                                      kBitrateNominalSize,
+                                     false);
+
+  int bitrate_min = lEndianToInt(header.begin() + kBitrateMinPos,
+                                 header.begin() + kBitrateMinPos +
+                                                  kBitrateMinSize,
+                                 false);
+  // TODO convert ints to file types
   return true;
 }
 
