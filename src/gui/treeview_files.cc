5d33946ec37256a558bde56263736497c8156b11
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-18 21:37:25 +0800

    Populate column names

--- src/gui/treeview_files.cc
+++ src/gui/treeview_files.cc
@@ -5,9 +5,20 @@
 namespace Mp3Edit {
 namespace Gui {
 
+namespace {
+
+const char* kTitle = "Title";
+const char* kArtist = "Artist";
+const char* kAlbum = "Album";
+const char* kTrack = "Track";
+const char* kBitrate = "Bitrate";
+const char* kSamplingRate = "Sampling Rate";
+const char* kChannelMode = "Channel Mode";
+const char* kFilepath = "Path";
+
+}  // namespace
+
 TreeViewFiles::Columns::Columns() {
-  add(pos_);
-  add(filepath_);
   add(title_);
   add(artist_);
   add(album_);
@@ -15,6 +26,8 @@ TreeViewFiles::Columns::Columns() {
   add(bitrate_);
   add(sampling_rate_);
   add(channel_mode_);
+  add(filepath_);
+  add(pos_);
 }
 
 TreeViewFiles::TreeViewFiles(BaseObjectType* cobject,
@@ -22,6 +35,16 @@ TreeViewFiles::TreeViewFiles(BaseObjectType* cobject,
                              std::vector<File::File>& files)
     : Gtk::TreeView(cobject), files_(files) {
   liststore_ = Gtk::ListStore::create(columns_);
+  this->set_model(liststore_);
+  this->append_column(kTitle, columns_.title());
+  this->append_column(kArtist, columns_.artist());
+  this->append_column(kAlbum, columns_.album());
+  this->append_column(kTrack, columns_.track());
+  this->append_column(kBitrate, columns_.bitrate());
+  this->append_column(kSamplingRate, columns_.samplingRate());
+  this->append_column(kChannelMode, columns_.channelMode());
+  this->append_column(kFilepath, columns_.filepath());
+
 }
 
 void TreeViewFiles::populateTreeView() {
