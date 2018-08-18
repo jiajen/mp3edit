e9541445409122c9d027f2cda9f219c1e215483f
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-18 21:19:22 +0800

    Columns data

--- src/gui/treeview_files.cc
+++ src/gui/treeview_files.cc
@@ -1,7 +1,21 @@
 #include "mp3edit/src/gui/treeview_files.h"
 
+#include <gtkmm/liststore.h>
+
 namespace Mp3Edit {
 namespace Gui {
+
+TreeViewFiles::Columns::Columns() {
+  add(pos_);
+  add(filepath_);
+  add(title_);
+  add(artist_);
+  add(album_);
+  add(track_);
+  add(bitrate_);
+  add(sampling_rate_);
+  add(channel_mode_);
+}
 
 TreeViewFiles::TreeViewFiles(BaseObjectType* cobject,
                              const Glib::RefPtr<Gtk::Builder>&,
--- src/gui/treeview_files.h
+++ src/gui/treeview_files.h
@@ -2,10 +2,12 @@
 #define MP3EDIT_SRC_GUI_TREEVIEW_FILES_H_
 
 #include <vector>
+#include <string>
 
 #include <gtkmm/builder.h>
 #include <gtkmm/treeview.h>
 #include <gtkmm/liststore.h>
+#include <gtkmm/treemodelcolumn.h>
 
 #include "mp3edit/src/file.h"
 
@@ -18,7 +20,20 @@ class TreeViewFiles: public Gtk::TreeView {
                 std::vector<File::File>& files);
   void populateTreeView();
  private:
-  class Columns;
+  class Columns : public Gtk::TreeModel::ColumnRecord {
+   public:
+    Columns();
+   private:
+    Gtk::TreeModelColumn<int> pos_;
+    Gtk::TreeModelColumn<std::string> filepath_;
+    Gtk::TreeModelColumn<std::string> title_;
+    Gtk::TreeModelColumn<std::string> artist_;
+    Gtk::TreeModelColumn<std::string> album_;
+    Gtk::TreeModelColumn<std::string> track_;
+    Gtk::TreeModelColumn<std::string> bitrate_;
+    Gtk::TreeModelColumn<std::string> sampling_rate_;
+    Gtk::TreeModelColumn<std::string> channel_mode_;
+  };
 
   std::vector<File::File>& files_;
 
