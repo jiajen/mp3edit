8c1597fd7c30dff4e6f2cd1e7ffcbdf6b7ab99dd
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-20 00:45:53 +0800

    Only add track total for vorbis tag if track is existant

--- src/reader/tag/vorbis_shared.cc
+++ src/reader/tag/vorbis_shared.cc
@@ -224,8 +224,9 @@ Bytes generateTag(const std::string& title, const std::string& artist,
   addVorbisField(album, kCommentAlbumHeader, kCommentAlbumHeaderSize, tag);
   addVorbisField(track_num,
                  kCommentTrackNumHeader, kCommentTrackNumHeaderSize, tag);
-  addVorbisField(track_denum,
-                 kCommentTrackTotalHeader, kCommentTrackTotalHeaderSize, tag);
+  if (track_num != -1)
+    addVorbisField(track_denum,
+                   kCommentTrackTotalHeader, kCommentTrackTotalHeaderSize, tag);
 
   if (has_framing_bit) tag.push_back(0x01);
 
