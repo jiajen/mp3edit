ce207562cdd88f323ef0c8709967b10e07022f49
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-20 21:23:41 +0800

    Expose underlying needed vector functions for the Files class

--- src/files.h
+++ src/files.h
@@ -14,6 +14,9 @@ class Files {
  public:
   Files();
   Files(const std::string& directory, bool recurse, bool read_audio_data);
+  inline std::vector<File::File>::iterator begin() { return files_.begin(); }
+  inline std::vector<File::File>::iterator end() { return files_.end(); }
+  inline File::File& operator[](int idx) { return files_[idx]; }
  private:
   std::vector<File::File> files_;
 };
