74d1324094c6f898c5b0744e7cc2951bf9c69fd0
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-20 23:23:18 +0800

    Handle if current row is invalid after saving

--- src/gui/treeview_files.cc
+++ src/gui/treeview_files.cc
@@ -170,8 +170,20 @@ void TreeViewFiles::saveSelectedFile(Gtk::TreeModel::Row& row,
   if (is_single_file) files_.clearErrorLog();
   int pos = row[columns_.pos()];
   if (!files_.saveFile(pos, rename_file)) {
-    // TODO remove invalid row. consider also if row is current row.
+    bool disable_signals_state = disable_signals_;
+    disable_signals_ = true;
+    if (row == *current_row_) {
+      treeselection_->unselect(current_row_);
+      current_row_ = Gtk::TreeModel::iterator();
+      entry_song_title_->set_text(Glib::ustring());
+      entry_song_artist_->set_text(Glib::ustring());
+      entry_song_album_->set_text(Glib::ustring());
+      entry_song_track_num_->set_text(Glib::ustring());
+      entry_song_track_denum_->set_text(Glib::ustring());
+    }
+    liststore_->erase(row);
     // TODO show error if is_single_file.
+    disable_signals_ = disable_signals_state;
     return;
   }
   row[columns_.filepath()] = files_[pos].getFilepath();
