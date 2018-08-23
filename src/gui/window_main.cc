04e057e97c76986a23e0909d8af8dd87e19a5645
Jia Jen Ng <jiajen.git@gmail.com>
2018-08-23 22:12:45 +0800

    Reordering variables and clearer signalling

--- src/gui/window_main.cc
+++ src/gui/window_main.cc
@@ -13,73 +13,88 @@ WindowMain::WindowMain(BaseObjectType* cobject,
     : Gtk::Window(cobject), files_(&dispatcher_),
       processing_mode_(Files::Files::ProcessingMode::kReady),
       builder_(builder) {
+  dispatcher_.connect(
+    sigc::mem_fun(*this, &WindowMain::onOperationUpdate));
+
+  builder_->get_widget("entry_dir", entry_dir_);
+
+  entry_dir_->signal_activate().connect(
+    sigc::mem_fun(*this, &WindowMain::onDirEntryEnterPress));
+
   builder_->get_widget("checkbox_read_subdir", checkbox_read_subdir_);
-  builder_->get_widget("checkbox_read_audio", checkbox_read_audio_);
 
   builder_->get_widget("btn_dir_open", btn_dir_open_);
   btn_dir_open_->signal_clicked().connect(
-    sigc::mem_fun(*this, &WindowMain::openDirDialog));
+    sigc::mem_fun(*this, &WindowMain::onOpenDirBtnPress));
 
   builder_->get_widget("btn_dir_refresh", btn_dir_refresh_);
   btn_dir_refresh_->signal_clicked().connect(
-    sigc::mem_fun(*this, &WindowMain::loadEntryDir));
+    sigc::mem_fun(*this, &WindowMain::onRefreshDirBtnPress));
 
-  builder_->get_widget("entry_dir", entry_dir_);
-  entry_dir_->signal_activate().connect(
-    sigc::mem_fun(*this, &WindowMain::loadEntryDir));
-
+  // TODO fix ordering when funcionality has been separated.
   builder_->get_widget("entry_song_title", entry_song_title_);
   builder_->get_widget("entry_song_artist", entry_song_artist_);
   builder_->get_widget("entry_song_album", entry_song_album_);
   builder_->get_widget("entry_song_track_num", entry_song_track_num_);
   builder_->get_widget("entry_song_track_denum", entry_song_track_denum_);
-
   builder_->get_widget("progressbar_main", progressbar_main_);
   progressbar_main_->set_show_text(true);
   progressbar_main_->set_text("");
-  builder_->get_widget("btn_cancel_action", btn_cancel_action_);
-  btn_cancel_action_->signal_clicked().connect(
-    sigc::mem_fun(*this, &WindowMain::onCancelBtnPress));
-
   builder_->get_widget_derived("gtk_treeview_files", treeview_files_, files_,
                                entry_song_title_, entry_song_artist_,
                                entry_song_album_, entry_song_track_num_,
                                entry_song_track_denum_, progressbar_main_);
 
-  builder_->get_widget("btn_song_search_web", btn_song_search_web_);
-  btn_song_search_web_->signal_clicked().connect(
-    sigc::mem_fun(*this, &WindowMain::onSearchWebBtnPress));
+  builder_->get_widget("checkbox_read_audio", checkbox_read_audio_);
+  builder_->get_widget("checkbox_rename_file", checkbox_rename_file_);
 
   builder_->get_widget("btn_song_save_single", btn_song_save_single_);
   btn_song_save_single_->signal_clicked().connect(
     sigc::mem_fun(*this, &WindowMain::onSaveFileBtnPress));
 
+  builder_->get_widget("btn_song_search_web", btn_song_search_web_);
+  btn_song_search_web_->signal_clicked().connect(
+    sigc::mem_fun(*this, &WindowMain::onSearchWebBtnPress));
+
   builder_->get_widget("btn_song_save_all", btn_song_save_all_);
   btn_song_save_all_->signal_clicked().connect(
     sigc::mem_fun(*this, &WindowMain::onSaveAllFilesBtnPress));
 
-  builder_->get_widget("checkbox_rename_file", checkbox_rename_file_);
-
-  dispatcher_.connect(
-    sigc::mem_fun(*this, &WindowMain::onOperationUpdate));
+  builder_->get_widget("btn_cancel_action", btn_cancel_action_);
+  btn_cancel_action_->signal_clicked().connect(
+    sigc::mem_fun(*this, &WindowMain::onCancelBtnPress));
 }
 
-void WindowMain::toggleLoadingMode(bool enter_loading_mode) {
-  checkbox_read_subdir_->set_sensitive(!enter_loading_mode);
-  checkbox_read_audio_->set_sensitive(!enter_loading_mode);
-  btn_dir_open_->set_sensitive(!enter_loading_mode);
-  btn_dir_refresh_->set_sensitive(!enter_loading_mode);
-  entry_dir_->set_sensitive(!enter_loading_mode);
-  treeview_files_->set_sensitive(!enter_loading_mode);
-  entry_song_title_->set_sensitive(!enter_loading_mode);
-  entry_song_artist_->set_sensitive(!enter_loading_mode);
-  entry_song_album_->set_sensitive(!enter_loading_mode);
-  entry_song_track_num_->set_sensitive(!enter_loading_mode);
-  entry_song_track_denum_->set_sensitive(!enter_loading_mode);
-  btn_song_save_single_->set_sensitive(!enter_loading_mode);
-  btn_song_save_all_->set_sensitive(!enter_loading_mode);
-  btn_song_search_web_->set_sensitive(!enter_loading_mode);
-  checkbox_rename_file_->set_sensitive(!enter_loading_mode);
+void WindowMain::onDirEntryEnterPress() {
+  loadEntryDir();
+}
+
+void WindowMain::onOpenDirBtnPress() {
+  openDirDialog();
+}
+
+void WindowMain::onRefreshDirBtnPress() {
+  loadEntryDir();
+}
+
+void WindowMain::onSaveFileBtnPress() {
+  treeview_files_->saveSelectedFile(checkbox_rename_file_->get_active());
+}
+
+void WindowMain::onSearchWebBtnPress() {
+  // TODO
+}
+
+void WindowMain::onSaveAllFilesBtnPress() {
+  treeview_files_->saveAllFiles(checkbox_rename_file_->get_active());
+}
+
+void WindowMain::onCancelBtnPress() {
+  files_.stopOperation();
+}
+
+void WindowMain::onOperationUpdate() {
+  // TODO
 }
 
 void WindowMain::openDirDialog() {
@@ -109,24 +124,22 @@ void WindowMain::loadEntryDir() {
   treeview_files_->populateTreeView();
 }
 
-void WindowMain::onSaveFileBtnPress() {
-  treeview_files_->saveSelectedFile(checkbox_rename_file_->get_active());
-}
-
-void WindowMain::onSaveAllFilesBtnPress() {
-  treeview_files_->saveAllFiles(checkbox_rename_file_->get_active());
-}
-
-void WindowMain::onSearchWebBtnPress() {
-  // TODO
-}
-
-void WindowMain::onCancelBtnPress() {
-  files_.stopOperation();
-}
-
-void WindowMain::onOperationUpdate() {
-  // TODO
+void WindowMain::toggleLoadingMode(bool enter_loading_mode) {
+  checkbox_read_subdir_->set_sensitive(!enter_loading_mode);
+  checkbox_read_audio_->set_sensitive(!enter_loading_mode);
+  btn_dir_open_->set_sensitive(!enter_loading_mode);
+  btn_dir_refresh_->set_sensitive(!enter_loading_mode);
+  entry_dir_->set_sensitive(!enter_loading_mode);
+  treeview_files_->set_sensitive(!enter_loading_mode);
+  entry_song_title_->set_sensitive(!enter_loading_mode);
+  entry_song_artist_->set_sensitive(!enter_loading_mode);
+  entry_song_album_->set_sensitive(!enter_loading_mode);
+  entry_song_track_num_->set_sensitive(!enter_loading_mode);
+  entry_song_track_denum_->set_sensitive(!enter_loading_mode);
+  btn_song_save_single_->set_sensitive(!enter_loading_mode);
+  btn_song_save_all_->set_sensitive(!enter_loading_mode);
+  btn_song_search_web_->set_sensitive(!enter_loading_mode);
+  checkbox_rename_file_->set_sensitive(!enter_loading_mode);
 }
 
 }  // namespace Gui
--- src/gui/window_main.h
+++ src/gui/window_main.h
@@ -20,35 +20,40 @@ class WindowMain: public Gtk::Window {
   WindowMain(BaseObjectType* cobject,
              const Glib::RefPtr<Gtk::Builder>& builder);
  private:
-  void openDirDialog();
-  void loadEntryDir();
+  void onDirEntryEnterPress();
+  void onOpenDirBtnPress();
+  void onRefreshDirBtnPress();
   void onSaveFileBtnPress();
+  void onSearchWebBtnPress();
   void onSaveAllFilesBtnPress();
   void onCancelBtnPress();
-  void onSearchWebBtnPress();
-  void toggleLoadingMode(bool enter_loading_mode);
+
   void onOperationUpdate();
+
+  void openDirDialog();
+  void loadEntryDir();
+  void toggleLoadingMode(bool enter_loading_mode);
 
   Glib::Dispatcher dispatcher_;
   Files::Files files_;
   Files::Files::ProcessingMode processing_mode_;
 
   Glib::RefPtr<Gtk::Builder> builder_;
+  Gtk::Entry* entry_dir_;
   Gtk::CheckButton* checkbox_read_subdir_;
-  Gtk::CheckButton* checkbox_read_audio_;
   Gtk::Button* btn_dir_open_;
   Gtk::Button* btn_dir_refresh_;
-  Gtk::Entry* entry_dir_;
   TreeViewFiles* treeview_files_;
+  Gtk::CheckButton* checkbox_read_audio_;
   Gtk::Entry* entry_song_title_;
   Gtk::Entry* entry_song_artist_;
   Gtk::Entry* entry_song_album_;
   Gtk::Entry* entry_song_track_num_;
   Gtk::Entry* entry_song_track_denum_;
-  Gtk::Button* btn_song_search_web_;
-  Gtk::Button* btn_song_save_single_;
-  Gtk::Button* btn_song_save_all_;
   Gtk::CheckButton* checkbox_rename_file_;
+  Gtk::Button* btn_song_save_single_;
+  Gtk::Button* btn_song_search_web_;
+  Gtk::Button* btn_song_save_all_;
   Gtk::ProgressBar* progressbar_main_;
   Gtk::Button* btn_cancel_action_;
 };
